ba2b301130ea48d24831012a03d34e14ebbbc856
PPHelios <103271591+PPHelios@users.noreply.github.com>
2022-05-07 16:05:30 +0200

    Update app.js
    Added dynamic navbar and the active state

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -22,15 +22,26 @@
  * Define Global Variables
  * 
 */
-
+const sections = Array.from(document.querySelectorAll("section"));
+ const navBarItems = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
+function createListItems () {
+        for (section of sections) {
+        
+// create a list item for the section
+listItem = document.createElement("li");
+// insert the list item attributes
+listItem.innerHTML = `<li><a href="#${section.id}" class="menu__link" data-nav="${section.id}">${section.dataset.nav}</a></li>`;
+//
+navBarItems.appendChild(listItem)          
+        }
+}
+createListItems()
 
 /**
  * End Helper Functions
@@ -40,9 +51,34 @@
 
 // build the nav
 
-
+document.addEventListener("scroll" ,function(){
+        sections.forEach(function(active){
+                if  (active.getBoundingClientRect().top<400 && active.getBoundingClientRect().bottom>400)
+                    
+            {active.classList.add("your-active-class");
+                        
+                }else {active.classList.remove("your-active-class");
+                        
+                }
+        })
+})
 // Add class 'active' to section when near top of viewport
 
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
 
 // Scroll to anchor ID using scrollTO event
 
